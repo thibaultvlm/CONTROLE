c0a2c40dc10ce9f4a8673b34a6f79c1fc8693a7a
thibaultvlm <vuillaume.thibault55@gmail.com>
2025-12-18 15:22:09 +0100

    Add route origin in index.routes.js & create a details & detailsstudio.html

--- server/endpoint/routes/index.routes.js
+++ server/endpoint/routes/index.routes.js
@@ -4,5 +4,7 @@ const gameRoute = require('./games.routes');
 const router = express.Router();
 router.use('/games', gameRoute);
 // router.use('/studiogames', sutdioRoute);
+router.use('/developers', developerRoute);
+
 module.exports = router;
 
