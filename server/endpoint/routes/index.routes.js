ae83adf0a9bf434e79542441a4d2cbe370109bd0
thibaultvlm <vuillaume.thibault55@gmail.com>
2025-12-19 19:01:42 +0100

    Refactor game and studio services; remove unused code and improve error handling
    - Cleaned up the GameService and StudioService classes by removing commented-out code and redundant error handling.
    - Updated the methods for fetching, adding, modifying, and deleting games and studios to streamline the code.
    - Removed the details.html file as it was no longer needed.
    - Adjusted the HTML files for games and studios to ensure consistency and remove unnecessary sections.
    - Improved the structure of the server-side controllers for games and studios, enhancing readability and maintainability.

--- server/endpoint/routes/index.routes.js
+++ server/endpoint/routes/index.routes.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const gameRoute = require('./games.routes');
-<<<<<<< HEAD
 const studioRoute = require('./studio.routes');
 const developerRoute = require('./developers.routes');
-=======
-const studioRoute = require('./studio.routes')
-const developerRoute = require('./developers.routes')
->>>>>>> 9c1dd51274dd08343d1ae12a3584582ee1006cac
 
 const router = express.Router();
 
--- server/endpoint/routes/studio.routes.js
+++ server/endpoint/routes/studio.routes.js
@@ -2,11 +2,7 @@ const express = require('express');
 const studioCtrl = require('../controller/studio.controller');
 const router = express.Router();
 
-<<<<<<< HEAD
 // Configuration des routes pour les studios
-=======
-
->>>>>>> 9c1dd51274dd08343d1ae12a3584582ee1006cac
 router.post('/', studioCtrl.newStudio);
 router.get('/', studioCtrl.getAllStudios);
 router.get('/:id', studioCtrl.getOneStudio);
